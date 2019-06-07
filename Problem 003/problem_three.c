39933b577506ff19f566cde0e4ee7781f2bc681c
Milan Capoor <capoorm123@gmail.com>
2019-06-07 19:19:32 -0400

    Fix Problem 3

--- Problem 003/problem_three.c
+++ Problem 003/problem_three.c
@@ -8,8 +8,8 @@ What is the largest prime factor of the number 600851475143 ?
 #include <math.h>
 #include <stdlib.h>
 
-int check_primality(number) { //determine if a given number is prime
-    for (int i = 2; i < sqrt(number); i++) { //check each divisor up to the square root
+long long int check_primality(number) { //determine if a given number is prime
+    for (long long int i = 2; i <= sqrt(number); i++) { //check each divisor up to the square root
         if (number % i == 0) { //if any number is a factor
            return 0; //return 0 (false)
         }   
@@ -18,12 +18,16 @@ int check_primality(number) { //determine if a given number is prime
 }
 
 int main() {
-    long long int number = 600851475143;
-
-    for (int i = 2; i < sqrt(number); i++) { //test all ints up to the sqrt of the number
-        if (number % i == 0) { //if 'i' is a factor 
-            if (check_primality(i) == 1) { //check if i is prime
-                printf("%d is a prime factor of 600851475143\n", i); //print the prime factors
+    long long int number = 9223372036854775806;
+    
+    if (check_primality(number) == 1) {
+        printf("The number is prime.\n");
+    } else {
+        for (long long int i = 2; i <= sqrt(number); i++) { //test all ints up to the sqrt of the number
+            if (number % i == 0) { //if 'i' is a factor 
+                if (check_primality(i) == 1) { //check if i is prime
+                    printf("%llu is a prime factor of 600851475143\n", i); //print the unique prime factors
+                } 
             }
         }
     }
