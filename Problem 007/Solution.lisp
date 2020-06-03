6609be6d176fda9291fde5e684548ecc372c22c5
fried-man <andrewfriedman01@gmail.com>
2020-06-02 23:41:37 -0400

    Updated solution #007 for speed

--- Problem 007/Solution.lisp
+++ Problem 007/Solution.lisp
@@ -2,19 +2,21 @@
   Problem #: 007
   Author: Andrew Friedman
   Created: 01/07/2020
+  Edited: 06/02/2020
 ||#
 
-(setq *total* 1) ; how many primes found
-(setq *highest* 2) ; TBD answer
-(loop for x from 3 ; go from 2 to infinity
+(defun isPrime (n) ; checks if prime (takes integer)
+  (loop for x from 3 to (sqrt n) by 2 ; iterate through possible factors
+    do (if (= (mod n x) 0) ; check if evenly divisible
+         (return-from isPrime nil))) ; return false
+  (return-from isPrime t)) ; return true
+
+(setq *total* 1) ; how many primes found TODO: check
+(setq *highest* 3) ; TBD answer
+(loop for x from 3 by 2 ; go from 3 to infinity += 2
   until (= *total* 10001) ; find 10001st prime
-  do (setq *prime* t) ; assume prime
-  do (loop for y from 2 to x ; go through all possible factors
-      until (or (not *prime*) (= x y)) ; stop when not prime or max reached
-      do (if (= (mod x y) 0) ; if evenly divisible
-           (setq *prime* nil))) ; x isnt prime
-  do (if *prime* ; if still prime
+  do (if (isPrime x) ; check if x is prime
        (progn ; multiple instructions in if
-        (setq *total* (+ *total* 1)) ; total++
-        (setq *highest* x)))) ; set newest prime
+         (setq *total* (+ *total* 1)) ; total++
+         (setq *highest* x)))) ; set newest prime
 (print *highest*) ; print answer :)
